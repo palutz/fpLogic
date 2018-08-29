28ecee72ce331a0c0015cc0a13601e3e785251e1
palutz <stefano.paluello@gmail.com>
2018-08-29 15:00:21 +0100

    comments and ldf condition

--- 01_prime.hs
+++ 01_prime.hs
@@ -2,6 +2,13 @@ module GS
 
 where 
 
+-- Prime number
+--
+-- Prime number test 
+-- Proposition to implemtent the prime number test:
+-- 1) if n > 1 then LD(n) is a prime number
+-- 2) if n > 1 and n is not prime, then (LD(n))^2 <= n
+
 -- divdes denominator numbertodivide 
 divides :: Integer -> Integer -> Bool
 divides d n = rem n d == 0
@@ -13,7 +20,7 @@ ld n = ldf 2 n
 -- (inner) least divisor function implementation
 ldf :: Integer -> Integer -> Integer
 ldf k n | divides k n = k
-        | k^2 >= n = n
+        | k^2 > n = n
         | otherwise = ldf (k+1) n
 
 -- prime0 - first implementation of the prime number function
