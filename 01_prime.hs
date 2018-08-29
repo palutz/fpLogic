1dd3010fbcc2a1fc5b9d7b02972a989acebc41bd
palutz <stefano.paluello@gmail.com>
2018-08-29 13:38:47 +0100

    ld and ldf functions

---
+++ 01_prime.hs
@@ -0,0 +1,12 @@
+module GS
+
+where 
+
+divides :: Integer -> Integer -> Bool
+divides d n = rem n d == 0
+
+ld n = ldf 2 n
+
+ldf k n | divides k n = k
+        | k^2 > n = n
+        | otherwise = ldf (k+1) n
