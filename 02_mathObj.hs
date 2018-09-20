a36e081b1e17860967ee655f354cd28f38c8c379
palutz <stefano.paluello@gmail.com>
2018-09-20 12:44:48 +0100

    chapter 02 start

---
+++ 02_mathObj.hs
@@ -0,0 +1,10 @@
+-- Chapter 02 - Mathematical Objects
+
+module Chapter02
+
+where 
+
+-- logical connectives:
+-- not, if, and or, if and only if (iff), for all, for some 
+
+data Bool' = False | True
