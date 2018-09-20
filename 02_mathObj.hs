7891710a1a31419423b38db80789b95ea42d03fb
palutz <stefano.paluello@gmail.com>
2018-09-20 12:56:10 +0100

    or, and

--- 02_mathObj.hs
+++ 02_mathObj.hs
@@ -7,4 +7,17 @@ where
 -- logical connectives:
 -- not, if, and or, if and only if (iff), for all, for some 
 
-data Bool' = False | True
+-- data Bool = False | True
+
+
+-- Negation
+-- not :: Bool -> Bool
+not1 :: Bool -> Bool
+not1 True = False
+not1 False = True
+
+-- Conjunction 
+-- and :: Bool -> Bool -> Bool 
+(&&!) :: Bool -> Bool -> Bool 
+False &&! x = False
+True  &&! x = x
