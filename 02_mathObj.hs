623dbcf8148cb62d06000108cb07eed6f8e3966e
palutz <stefano.paluello@gmail.com>
2018-09-21 09:04:45 +0100

    if ..

--- 02_mathObj.hs
+++ 02_mathObj.hs
@@ -7,8 +7,8 @@ where
 -- logical connectives:
 -- not, if, and or, if and only if (iff), for all, for some 
 
+-- example of declaration (already declared in prelude
 -- data Bool = False | True
-
 
 -- Negation
 -- not :: Bool -> Bool
@@ -21,3 +21,22 @@ not1 False = True
 (&&!) :: Bool -> Bool -> Bool 
 False &&! x = False
 True  &&! x = x
+
+-- Disjunction
+-- or :: Bool -> Bool -> Bool
+(||!) :: Bool -> Bool -> Bool
+True  ||! x = True
+False ||! x = x
+
+-- Implication 
+-- if P then Q or Q if P; implication of P and Q
+-- implication is true if 
+--    both P (antecedent) and Q (consequent) are false
+--    P (antecedent) is false and Q (consequent) is true
+--    both antecedent and consequent are true
+-- if :: Bool -> Bool -> Bool
+(==>) :: Bool -> Bool -> Bool
+-- x ==> y = not x ||! y
+-- or other implementation
+True  ==> x = x
+False ==> x = True
